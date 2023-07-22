f6d159130fc13b04be45b4a2480d612baf389435
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-22 14:37:23 -0500

    Initial setup.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={/* code */ <div>Home Page</div>} />
+        <Route
+          path="testing"
+          element={
+            <div>
+              <h2>testing</h2>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
